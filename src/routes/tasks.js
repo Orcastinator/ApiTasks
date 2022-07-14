3564df19807e1f436464c7dd512d2ac312c2cdc5
Orcastinator <lorenzo.martinez.pablo@gmail.com>
2022-07-14 15:59:46 +0200

    SQL updated

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -1,35 +1,61 @@
 const express = require('express');
 const router = express.Router();
 
+const mysql = require('mysql2');
+const poolDB = mysql.createPool({
+    host: process.env.DB_HOST,
+    database: process.env.DB_BBDD,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+});
+
+
+
 const tasks = require('../tasks.json');
 
 router.get("/", (req, res) => {
-    res.json(tasks);
+    poolDB.query(
+        "SELECT * FROM tasks",
+        (err, results, fields) => {
+            if (results){
+                res.json(results);
+            }
+            else{
+                res.send("Empty.");
+            }
+        });
 });
 
 router.get('/:id', (req, res) => {
     var {id} = req.params;
-    var exito = 0;
-    tasks.forEach((t, i) => {
-        if (t.id == id){
-            res.json(t);
-            exito = 1;
-        }
-    });
-    if (exito == 0){
-        res.status(500).send("Doesnt exists");
-    }
+    poolDB.query(
+        "SELECT * FROM tasks WHERE id_task = "+id,
+        (err, results, fields) => {
+            if (results.length > 0){
+                res.json(results);
+            }
+            else{
+                res.send("Doesnt exist.");
+            }
+        });
 });
 
 router.post('/', (req, res) => {
     var t = req.body;
     if (t.titulo && t.descripcion){
-        t.id = tasks.length+1;
-        t.active = false;
-        t.done = false;
-        t.dateDone = "";
-        res.send("Task added");
-        tasks.push(t);
+        poolDB.query(
+            "INSERT INTO tasks (titulo, descripcion) VALUES ('"+t.titulo+"', '"+t.descripcion+"')",
+            (err, results, fields) => {
+                if (results.affectedRows > 0){
+                    res.send("Task added.");
+                }
+                else{
+                    res.send("Error adding task.");
+                }
+            });
     }
     else {
         res.status(500).send("Incomplete task");
@@ -38,36 +64,31 @@ router.post('/', (req, res) => {
 
 router.delete('/:id', (req, res) => {
     var {id} = req.params;
-    var exito = 0;
-    tasks.forEach((t, i) => {
-        if (t.id == id){
-            res.send("Task deleted");
-            tasks.splice(i, 1);
-            exito = 1;
-        }
-    });
-    if (exito == 0){
-        res.status(500).send("Doesnt exists");
-    }
+    poolDB.query(
+        "DELETE FROM tasks WHERE id_task = "+id,
+        (err, results, fields) => {
+            if (results.affectedRows > 0){
+                res.send("Task deleted.");
+            }
+            else{
+                res.send("Error deleting task.");
+            }
+        });
 });
 
 router.put('/:id', (req, res) => {
-    var exito = 0;
     var {id} = req.params;
     var o = {...req.body};
-    if (o.titulo && o.descripcion){
-        tasks.forEach((t, i) => {
-            if (t.id == id){
-                t.titulo = o.titulo;
-                t.descripcion = o.descripcion;
-                res.send("Task updated");
-                exito = 1;
+    poolDB.query(
+        "UPDATE tasks SET titulo = '"+o.titulo+"', descripcion = '"+o.descripcion+"' WHERE id_task = "+id,
+        (err, results, fields) => {
+            if (results.affectedRows > 0){
+                res.send("Task updated.");
+            }
+            else{
+                res.send("Error updating task.");
             }
         });
-    }
-    if (exito == 0){
-        res.status(500).send("Doesnt exists");
-    }
 });
 
 module.exports = router;
