a2d4da9eba045eb4bf2b8e7de2f6b123b97f34fb
Orcastinator <lorenzo.martinez.pablo@gmail.com>
2022-07-14 08:21:46 +0200

    Post added

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -1,9 +1,35 @@
 const express = require('express');
-
 const router = express.Router();
 
+const tasks = require('../tasks.json');
+
 router.get("/", (req, res) => {
-    res.json({'msg':'GET method'});
+    res.json(tasks);
+});
+
+router.get('/:id', (req, res) => {
+    var {id} = req.params;
+    tasks.forEach((t, i) => {
+        if (t.id == id){
+            res.json(t);
+        }
+    });
+    res.status(500).send("Doesnt exists");
+});
+
+router.post('/', (req, res) => {
+    var t = req.body;
+    if (t.titulo && t.descripcion){
+        t.id = tasks.length+1;
+        t.active = false;
+        t.done = false;
+        t.dateDone = "";
+        res.send("Task added");
+        tasks.push(t);
+    }
+    else {
+        res.status(500).send("Incomplete task");
+    }
 });
 
 module.exports = router;
