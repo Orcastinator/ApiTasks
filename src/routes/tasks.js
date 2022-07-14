ad565512064052b30c9f4dbe4f0de5a7ee951c06
Orcastinator <lorenzo.martinez.pablo@gmail.com>
2022-07-14 08:36:16 +0200

    Put, Delete added

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -9,12 +9,16 @@ router.get("/", (req, res) => {
 
 router.get('/:id', (req, res) => {
     var {id} = req.params;
+    var exito = 0;
     tasks.forEach((t, i) => {
         if (t.id == id){
             res.json(t);
+            exito = 1;
         }
     });
-    res.status(500).send("Doesnt exists");
+    if (exito == 0){
+        res.status(500).send("Doesnt exists");
+    }
 });
 
 router.post('/', (req, res) => {
@@ -29,6 +33,40 @@ router.post('/', (req, res) => {
     }
     else {
         res.status(500).send("Incomplete task");
+    }
+});
+
+router.delete('/:id', (req, res) => {
+    var {id} = req.params;
+    var exito = 0;
+    tasks.forEach((t, i) => {
+        if (t.id == id){
+            res.send("Task deleted");
+            tasks.splice(i, 1);
+            exito = 1;
+        }
+    });
+    if (exito == 0){
+        res.status(500).send("Doesnt exists");
+    }
+});
+
+router.put('/:id', (req, res) => {
+    var exito = 0;
+    var {id} = req.params;
+    var o = {...req.body};
+    if (o.titulo && o.descripcion){
+        tasks.forEach((t, i) => {
+            if (t.id == id){
+                t.titulo = o.titulo;
+                t.descripcion = o.descripcion;
+                res.send("Task updated");
+                exito = 1;
+            }
+        });
+    }
+    if (exito == 0){
+        res.status(500).send("Doesnt exists");
     }
 });
 
